c71da17fae4f346b84c64cab2695e7879d1df102
Edgar Maniega Medrano <maniega@outlook.es>
2024-07-30 01:58:10 +0200

    Using `mysql2` module for using MySql 8.0

--- proyecto/Api_Express/index.js
+++ proyecto/Api_Express/index.js
@@ -1,13 +1,13 @@
 // Importación de módulos necesarios
 const express = require("express"); // Framework para manejar el servidor y las rutas
-const mysql = require("mysql"); // Cliente MySQL para interactuar con la base de datos
+const mysql = require("mysql2"); // Cliente MySQL para interactuar con la base de datos
 const bodyParser = require("body-parser"); // Middleware para parsear el cuerpo de las solicitudes
 const cors = require('cors'); //Importante requerir cors y utilizarlo para permisos
 
 const app = express(); // Creación de una instancia de Express
 
 // Middleware para configurar CORS (Cross-Origin Resource Sharing)
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.setHeader("Acess-Control-Allow-Origin", "*"); // Permite el acceso desde cualquier origen
   res.setHeader("Acess-Control-Allow-methods", "*"); // Permite cualquier método HTTP
   next(); // Pasa al siguiente middleware
@@ -24,7 +24,7 @@ const conexion = mysql.createConnection({
   host: "localhost",
   database: "proyecto",
   user: "root",
-  password: "",
+  password: "admin",
 });
 
 // Inicia el servidor y escucha en el puerto especificado
@@ -40,7 +40,8 @@ conexion.connect((error) => {
 
 // Ruta raíz que responde con "API"
 app.get("/", (req, res) => {
-  res.send("API");
+
+  res.sendFile("../Tienda/src/index.html", { root: __dirname });
 });
 
 // Ruta para obtener todos los usuarios
@@ -80,28 +81,28 @@ app.post("/usuarios/agregar", (req, res) => {
     apellidos: req.body.apellidos,
     email: req.body.email,
     password: req.body.password,
-   
+
   }; // Crea un objeto usuario con los datos del cuerpo de la solicitud
 
   const query = `INSERT INTO usuarios SET ?`; // Consulta SQL para insertar un nuevo usuario
   conexion.query(query, usuario, (error, resultado) => {
     if (error) return console.error(error.message); // Muestra el error si la consulta falla
 
-    res.json(`Se insertó correctamente el usuario`); 
+    res.json(`Se insertó correctamente el usuario`);
   });
 });
 
 // Ruta para actualizar un usuario por su ID
 app.put("/usuarios/actualizar/:id", (req, res) => {
   const { id } = req.params; // Obtiene el ID de los parámetros de la ruta
-  const { nombre, email,apellidos, password } = req.body; // Obtiene los datos del cuerpo de la solicitud
+  const { nombre, email, apellidos, password } = req.body; // Obtiene los datos del cuerpo de la solicitud
 
   const query = `UPDATE usuarios SET nombre='${nombre}', email='${email}', apellidos='${apellidos}', password='${password}' WHERE id_usuario='${id}'`; // Consulta SQL para actualizar el usuario
 
   conexion.query(query, (error, resultado) => {
     if (error) return console.error(error.message); // Muestra el error si la consulta falla
 
-    res.json(`Se actualizó el usuario`); 
+    res.json(`Se actualizó el usuario`);
   });
 });
 
@@ -113,6 +114,6 @@ app.delete("/usuarios/borrar/:id", (req, res) => {
   conexion.query(query, (error, resultado) => {
     if (error) return console.error(error.message); // Muestra el error si la consulta falla
 
-    res.json("Se eliminó el usuario"); 
+    res.json("Se eliminó el usuario");
   });
 });
